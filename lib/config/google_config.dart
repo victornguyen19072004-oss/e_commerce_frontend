60a1f898cf8c9bdb2b7f51e1c24396d004775acb
victornguyen19072004 <victornguyen19072004@gmail.com>
2026-06-03 16:16:38 +0700

    New

--- lib/config/google_config.dart
+++ lib/config/google_config.dart
@@ -1,6 +1,6 @@
 class GoogleConfig {
-  static const String webClientId =
-      '790202574296-9qsllsor2jv2n4qmg7d38fi1p3p6netv.apps.googleusercontent.com';
-  static const String iOSClientId =
-      '790202574296-7fi2obusn34e5us92tpjhvif6bgfd3ea.apps.googleusercontent.com';
+ static const String webClientId =
+   '790202574296-9qsllsor2jv2n4qmg7d38fi1p3p6netv.apps.googleusercontent.com';
+ static const String iOSClientId =
+   '790202574296-7fi2obusn34e5us92tpjhvif6bgfd3ea.apps.googleusercontent.com';
 }
